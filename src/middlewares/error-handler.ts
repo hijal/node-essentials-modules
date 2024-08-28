c2bbf51368aef2f4ff43cf851c6ef97e0f299e38
hijal <hijikeshhijal@gmail.com>
2024-08-28 18:39:23 +0600

    add console

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,20 +1,16 @@
-import { Response, Request, NextFunction } from "express";
-import httpStatus from "http-status";
-import { CustomError } from "../errors/custom-error";
+import { Response, Request, NextFunction } from 'express';
+import httpStatus from 'http-status';
+import { CustomError } from '../errors/custom-error';
 
-export const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({
-      errors: err.serializeErrors(),
+      errors: err.serializeErrors()
     });
   }
 
+  console.error(err);
   res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
-    errors: [{ message: err.message }],
+    errors: [{ message: err.message }]
   });
 };
